28a9b34f9f5eb3993590f4a837b57ee713ccbbce
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-05-09 11:04:39 -0400

    Agrega las dependencias necesarias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
-    id("androidx.room") version "2.7.2" apply false
-    id("com.google.dagger.hilt.android") version "2.57.1"
+    id("androidx.room") version "2.7.2"
+    id("com.google.dagger.hilt.android") version "2.57.1" apply true
 }
 
 android {
@@ -31,15 +31,22 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
+    }
+
+    room {
+        schemaDirectory("$projectDir/schemas")
     }
 }
 
@@ -49,34 +56,36 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
 
-
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
-    //navegacion
+    // Navegacion
     implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation(libs.kotlin.serialization.json)
-    //room
+
+    // Room
     implementation("androidx.room:room-runtime:2.7.2")
     implementation(libs.androidx.room.common.jvm)
-    annotationProcessor("androidx.room:room-compiler:2.7.2")
     ksp("androidx.room:room-compiler:2.7.2")
     implementation("androidx.room:room-ktx:2.7.2")
+
+    // Icons
     implementation("androidx.compose.material:material-icons-extended")
 
-    //Hilt
+    // Hilt
     implementation("com.google.dagger:hilt-android:2.57.1")
     ksp("com.google.dagger:hilt-android-compiler:2.57.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
-    //Retrofit
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
     implementation("com.squareup.retrofit2:converter-moshi:3.0.0")
     implementation("com.squareup.okhttp3:logging-interceptor:5.1.0")
 
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
