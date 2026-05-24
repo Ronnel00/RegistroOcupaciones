97771f99cb50e7538c1259c9322c13061e44fee5
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-05-24 14:41:04 -0400

    Agrega los imports y actualiza la version a la db para el uso de horas extras

--- app/src/main/java/edu/ucne/registroocupaciones/data/database/OcupacionDb.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/database/OcupacionDb.kt
@@ -3,16 +3,23 @@ package edu.ucne.registroocupaciones.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.registroocupaciones.data.local.dao.EmpleadoDao
+import edu.ucne.registroocupaciones.data.local.dao.HoraExtraDao
 import edu.ucne.registroocupaciones.data.local.dao.OcupacionDao
 import edu.ucne.registroocupaciones.data.local.entities.EmpleadoEntity
+import edu.ucne.registroocupaciones.data.local.entities.HoraExtraEntity
 import edu.ucne.registroocupaciones.data.local.entities.OcupacionEntity
 
 @Database(
-    entities = [OcupacionEntity::class, EmpleadoEntity::class],
-    version = 2,
+    entities = [
+        OcupacionEntity::class,
+        EmpleadoEntity::class,
+        HoraExtraEntity::class
+    ],
+    version = 3,
     exportSchema = false
 )
 abstract class OcupacionDB : RoomDatabase() {
     abstract fun ocupacionDao(): OcupacionDao
     abstract fun empleadoDao(): EmpleadoDao
+    abstract fun horaExtraDao(): HoraExtraDao
 }
