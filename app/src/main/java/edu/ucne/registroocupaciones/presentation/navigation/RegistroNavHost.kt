3a6b571d4f0b973ce82815f13c38330ef0ed8603
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-06-02 21:18:57 -0400

    Agrega las Screen de los registros al NavHost

--- app/src/main/java/edu/ucne/registroocupaciones/presentation/navigation/RegistroNavHost.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/presentation/navigation/RegistroNavHost.kt
@@ -1,64 +1,54 @@
 package edu.ucne.registroocupaciones.presentation.navigation
 
-import androidx.compose.material3.*
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.*
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.*
 import androidx.navigation.toRoute
+import edu.ucne.registroocupaciones.presentation.empleado.EmpleadoAdaptiveScreen
 import edu.ucne.registroocupaciones.presentation.empleado.edit.EditEmpleadoScreen
-import edu.ucne.registroocupaciones.presentation.empleado.list.EmpleadoListScreen
+import edu.ucne.registroocupaciones.presentation.horaextra.HoraExtraAdaptiveScreen
 import edu.ucne.registroocupaciones.presentation.horaextra.edit.EditHoraExtraScreen
-import edu.ucne.registroocupaciones.presentation.horaextra.list.HoraExtraListScreen
 import edu.ucne.registroocupaciones.presentation.ocupacion.OcupacionMainScreen
-import kotlinx.coroutines.launch
 
 @Composable
 fun RegistroNavHost(navHostController: NavHostController) {
-    val scope = rememberCoroutineScope()
     val drawerState = rememberDrawerState(DrawerValue.Closed)
-
-    DrawerMenu(drawerState = drawerState, navHostController = navHostController) {
+    DrawerMenu(
+        drawerState = drawerState,
+        navHostController = navHostController
+    ) {
         NavHost(
             navController = navHostController,
             startDestination = Screen.OcupacionList
         ) {
             composable<Screen.OcupacionList> {
-                OcupacionMainScreen(
-                    onDrawer = { scope.launch { drawerState.open() } }
-                )
+                OcupacionMainScreen()
             }
-
+            composable<Screen.Ocupacion> {
+                OcupacionMainScreen()
+            }
             composable<Screen.EmpleadoList> {
-                EmpleadoListScreen(
-                    onDrawer = { scope.launch { drawerState.open() } },
-                    goToEmpleado = { id -> navHostController.navigate(Screen.Empleado(id)) },
-                    createEmpleado = { navHostController.navigate(Screen.Empleado(0)) }
-                )
+                EmpleadoAdaptiveScreen(onDrawer = {})
             }
-
             composable<Screen.Empleado> {
                 val args = it.toRoute<Screen.Empleado>()
                 EditEmpleadoScreen(
                     empleadoId = args.empleadoId,
                     onNavigateBack = { navHostController.navigateUp() },
-                    onDrawer = { scope.launch { drawerState.open() } }
+                    onDrawer = {}
                 )
             }
-
             composable<Screen.HoraExtraList> {
-                HoraExtraListScreen(
-                    onDrawer = { scope.launch { drawerState.open() } },
-                    goToHoraExtra = { id -> navHostController.navigate(Screen.HoraExtra(id)) },
-                    createHoraExtra = { navHostController.navigate(Screen.HoraExtra(0)) }
-                )
+                HoraExtraAdaptiveScreen(onDrawer = {})
             }
-
             composable<Screen.HoraExtra> {
                 val args = it.toRoute<Screen.HoraExtra>()
                 EditHoraExtraScreen(
                     horaExtraId = args.horaExtraId,
                     onNavigateBack = { navHostController.navigateUp() },
-                    onDrawer = { scope.launch { drawerState.open() } }
+                    onDrawer = {}
                 )
             }
         }
