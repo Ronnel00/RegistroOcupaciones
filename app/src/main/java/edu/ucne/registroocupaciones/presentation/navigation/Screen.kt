965b07820653753ff66660b82e4f73123fdbcb95
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-06-02 21:28:51 -0400

    Cambia Screen.kt a interface

--- app/src/main/java/edu/ucne/registroocupaciones/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/presentation/navigation/Screen.kt
@@ -2,11 +2,11 @@ package edu.ucne.registroocupaciones.presentation.navigation
 
 import kotlinx.serialization.Serializable
 
-sealed class Screen {
-    @Serializable data object OcupacionList : Screen()
-    @Serializable data class Ocupacion(val ocupacionId: Int) : Screen()
-    @Serializable data object EmpleadoList : Screen()
-    @Serializable data class Empleado(val empleadoId: Int) : Screen()
-    @Serializable data object HoraExtraList : Screen()
-    @Serializable data class HoraExtra(val horaExtraId: Int) : Screen()
+sealed interface Screen {
+    @Serializable data object OcupacionList : Screen
+    @Serializable data class Ocupacion(val ocupacionId: Int) : Screen
+    @Serializable data object EmpleadoList : Screen
+    @Serializable data class Empleado(val empleadoId: Int) : Screen
+    @Serializable data object HoraExtraList : Screen
+    @Serializable data class HoraExtra(val horaExtraId: Int) : Screen
 }
